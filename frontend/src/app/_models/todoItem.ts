fb3fd2117857cf2868f3e1ff940b934f9101cf5f
Kiattisak Phanphu <kiattisak.pa@outlook.com>
2021-01-28 18:11:55 +0700

    todo

---
+++ frontend/src/app/_models/todoItem.ts
@@ -0,0 +1,10 @@
+export interface TodoItem {
+  id: number;
+  subject: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  created_Date: Date;
+  updated_Date: Date;
+  isActive: string;
+}
